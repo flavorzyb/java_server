103efac739a8e901484248e5936ce685a4d6d31d
flavorzhu <haker-haker@163.com>
2014-11-09 01:19:36 +0800

    增加sock设置

--- src/main/java/com/zhuyanbin/server/ServerThread.java
+++ src/main/java/com/zhuyanbin/server/ServerThread.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 
 /**
  * @author admin
@@ -38,32 +39,47 @@ public class ServerThread extends Thread
 		    
 			while((_socket != null) && (_socket.isClosed() == false))
 			{
-				BufferedReader br = new BufferedReader(new InputStreamReader(_socket.getInputStream()));
-				String str = br.readLine().toLowerCase();
-		        System.out.println("client send data:" + str);
-		        if (str.equals("closed"))
-		        {
-		        	_socket.close();
-		        }
-		        else if (str.equals("sleep")) 
-		        {
-		        	sleep(6000);
-		        	String result ="receive data: sleep ok\n";
-		        	byte[] data = result.getBytes("UTF-8");
-		        	_socket.getOutputStream().write(data);
-				}
-		        else
-		        {
-		        	sleep(1000);
-		        	String result ="receive data: data ok\n";
-		        	byte[] data = result.getBytes("UTF-8");
-		        	_socket.getOutputStream().write(data);
-		        }
+			    int len = _socket.getInputStream().available();
+			    if (len > 0)
+			    {
+    				BufferedReader br = new BufferedReader(new InputStreamReader(_socket.getInputStream()));
+    				String str = br.readLine().toLowerCase();
+    		        System.out.println("client send data:" + str);
+    		        if (str.equals("closed"))
+    		        {
+    		        	_socket.close();
+    		        }
+    		        else if (str.equals("sleep")) 
+    		        {
+    		        	sleep(6000);
+    		        	String result ="receive data: sleep ok\n";
+    		        	byte[] data = result.getBytes("UTF-8");
+    		        	_socket.getOutputStream().write(data);
+    				}
+    		        else
+    		        {
+    		        	String result ="receive data: data ok\n";
+    		        	byte[] data = result.getBytes("UTF-8");
+    		        	_socket.getOutputStream().write(data);
+    		        }
+			    }
+			    else
+			    {
+			        sleep(200);
+			    }
 			}
-			
-			_app.decrConnection();
-			_app.printConnection();
         }
+		catch (SocketTimeoutException e)
+		{
+		    try
+            {
+                _socket.close();
+            }
+            catch (IOException e1)
+            {
+                e1.printStackTrace();
+            }
+		}
         catch (IOException e)
         {
 	        e.printStackTrace();
@@ -72,5 +88,10 @@ public class ServerThread extends Thread
         {
 	        e.printStackTrace();
         }
+		finally
+		{
+		    _app.decrConnection();
+            _app.printConnection();
+		}
 	}
 }
